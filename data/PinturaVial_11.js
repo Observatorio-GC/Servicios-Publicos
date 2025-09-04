dd4e91096cbfa7672378d2f486ffef88cf11e4aa
Ignacio Diaz Testa <igdiaz@godoycruz.gov.ar>
2025-09-04 10:09:10 -0300

    cambios zonas 04/09

--- data/PinturaVial_11.js
+++
@@ -1,1 +0,0 @@
-var json_PinturaVial_11 = {"type":"FeatureCollection","name":"PinturaVial_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/PodaMunicipalEnejecucion_10.js
@@ -0,0 +1,1 @@
+var json_PodaMunicipalEnejecucion_10 = {"type":"FeatureCollection","name":"PodaMunicipalEnejecucion_10","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
